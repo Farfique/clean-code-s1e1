08d770922efd0ecdb63dab3c45044978ed70b292
Anna Mikhailova <petunova@gmail.com>
2021-11-25 16:59:04 +0300

    refactor: change tag css selectors to class selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -18,9 +18,11 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.classList.add('task-item');
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add('done-input');
     //label
     var label=document.createElement("label");//label
     //input (text)
@@ -33,18 +35,19 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add('task', 'task-label');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add('task', 'task-input');
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add('edit', 'button');
 
-    deleteButton.className="delete";
+    deleteButton.classList.add('delete', 'button');
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add('delete-image');
     deleteButton.appendChild(deleteButtonImg);
 
 
